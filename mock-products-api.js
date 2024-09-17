3bf719de1aae47589c7fc42e96a1b1f77b00a544
gabriel carlos cavalcante <ttbr2888@gmail.com>
2024-09-17 00:53:01 -0300

    feat: implementa mock de produtos

---
+++ mock-products-api.js
@@ -0,0 +1,46 @@
+
+export default function mockProducts()  {
+  return {
+    "products": [
+      {
+        "id": 1,
+        "price": 12000,
+        "title": "Notebook Gamer ASUS TUF Gaming F15 FX507VU, Intel core i7 13620H, RTX 4050, 8GB RAM, SSD 512GB, 15,6 LED FHD 144Hz, KeepOS - FX507VU-LP151",
+        "description": "NOTEBOOK POWER",
+        "image_url": '/notebook.jpeg',
+        "quantity": 2
+      },
+      {
+        "id": 2,
+        "price": 8500,
+        "title": "iPhone 15 Pro Max Apple 256GB, Câmera Tripla 48MP, Tela 6.7, Azul Titânio - MU7A3BE/A",
+        "description": "SMARTPHONE WITH HIGH PERFORMANCE",
+        "image_url": "/celular.jpeg",
+        "quantity": 5
+      },
+      {
+        "id": 3,
+        "price": 3000,
+        "title": "Processador AMD Ryzen 7 5700G, 3.8GHz (4.6GHz Max Turbo), Cache 20MB, 8 Núcleos, 16 Threads, Vídeo Integrado, AM4 - 100-100000263BOX",
+        "description": "NOISE CANCELLING WIRELESS HEADPHONES",
+        "image_url": "/processador.jpeg",
+        "quantity": 10
+      },
+      {
+        "id": 3,
+        "price": 3000,
+        "title": "Processador AMD Ryzen 7 5700G, 3.8GHz (4.6GHz Max Turbo), Cache 20MB, 8 Núcleos, 16 Threads, Vídeo Integrado, AM4 - 100-100000263BOX",
+        "description": "NOISE CANCELLING WIRELESS HEADPHONES",
+        "image_url": "/processador.jpeg",
+        "quantity": 10
+      },{
+        "id": 3,
+        "price": 3000,
+        "title": "Processador AMD Ryzen 7 5700G, 3.8GHz (4.6GHz Max Turbo), Cache 20MB, 8 Núcleos, 16 Threads, Vídeo Integrado, AM4 - 100-100000263BOX",
+        "description": "NOISE CANCELLING WIRELESS HEADPHONES",
+        "image_url": "/processador.jpeg",
+        "quantity": 10
+      }
+    ]
+  }
+}
