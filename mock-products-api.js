2629ccf7b96984124cee1c59802b184878f183b7
gabriel carlos cavalcante <ttbr2888@gmail.com>
2024-09-18 20:32:02 -0300

    feat: arrumas os mocks

--- mock-products-api.js
+++ mock-products-api.js
@@ -4,15 +4,14 @@ export default function mockProducts()  {
     "products": [
       {
         "id": 1,
-        "price": 12000,
+        "price": 11999,
         "title": "Notebook Gamer ASUS TUF Gaming F15 FX507VU, Intel core i7 13620H, RTX 4050, 8GB RAM, SSD 512GB, 15,6 LED FHD 144Hz, KeepOS - FX507VU-LP151",
         "description": "NOTEBOOK POWER",
         "image_url": '/notebook.jpeg',
-        "quantity": 2
       },
       {
         "id": 2,
-        "price": 8500,
+        "price": 8499,
         "title": "iPhone 15 Pro Max Apple 256GB, Câmera Tripla 48MP, Tela 6.7, Azul Titânio - MU7A3BE/A",
         "description": "SMARTPHONE WITH HIGH PERFORMANCE",
         "image_url": "/celular.jpeg",
@@ -20,27 +19,32 @@ export default function mockProducts()  {
       },
       {
         "id": 3,
-        "price": 3000,
+        "price": 2999,
         "title": "Processador AMD Ryzen 7 5700G, 3.8GHz (4.6GHz Max Turbo), Cache 20MB, 8 Núcleos, 16 Threads, Vídeo Integrado, AM4 - 100-100000263BOX",
         "description": "NOISE CANCELLING WIRELESS HEADPHONES",
         "image_url": "/processador.jpeg",
-        "quantity": 10
+      }, 
+      {
+        "id": 4,
+        "price": 449,
+        "title": "Mouse Gamer sem fio Logitech G309 LIGHTSPEED com Switches LIGHTFORCE, Sensor HERO 25K, 6 botões programáveis, Preto - 910-007198",
+        "description": "Mouse Gamer sem fio Logitech G309 LIGHTSPEED com Switches LIGHTFORCE, Sensor HERO 25K, 6 botões programáveis, Preto - 910-007198",
+        "image_url": "/mouse.jpeg",
       },
       {
-        "id": 3,
-        "price": 3000,
-        "title": "Processador AMD Ryzen 7 5700G, 3.8GHz (4.6GHz Max Turbo), Cache 20MB, 8 Núcleos, 16 Threads, Vídeo Integrado, AM4 - 100-100000263BOX",
-        "description": "NOISE CANCELLING WIRELESS HEADPHONES",
-        "image_url": "/processador.jpeg",
-        "quantity": 10
-      },{
-        "id": 3,
-        "price": 3000,
-        "title": "Processador AMD Ryzen 7 5700G, 3.8GHz (4.6GHz Max Turbo), Cache 20MB, 8 Núcleos, 16 Threads, Vídeo Integrado, AM4 - 100-100000263BOX",
-        "description": "NOISE CANCELLING WIRELESS HEADPHONES",
-        "image_url": "/processador.jpeg",
-        "quantity": 10
-      }
+        "id": 5,
+        "price": 3499,
+        "title": "Caixa de Som Acústica JBL PartyBox Stage 320, Bluetooth, 240W, Até 18h, Resistente a Água, Preto - JBLPBSTAGE320BR",
+        "description": "MCaixa de Som Acústica JBL PartyBox Stage 320, Bluetooth, 240W, Até 18h, Resistente a Água, Preto - JBLPBSTAGE320BR",
+        "image_url": "/caixa-de-som.jpeg",
+      },
+      {
+        "id": 6,
+        "price": 799,
+        "title": "Cadeira Gamer KBM! Gaming CG700 de Tecido, Até 150 kg, Apoio de Braço 4D, Reclinável Preta - KGCG700PT",
+        "description": "Cadeira Gamer KBM! Gaming CG700 de Tecido, Até 150 kg, Apoio de Braço 4D, Reclinável Preta - KGCG700PT",
+        "image_url": "/cadeira-gamer.jpeg",
+      },
     ]
   }
 }
